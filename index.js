e9dd76127cdd2ed3de05edc326023e3366c89e13
1999eth <97752208+lyen1688@users.noreply.github.com>
2024-10-31 02:33:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,40 @@ app.post("/getWalletInfo", async (req, res) => {
     const result = { wallet_address: address };
 
     try {
-      const pointsResponse = await axios.get(`https://kx58j6x5me.execute-api.us-east-1.amazonaws.com/scroll/wallet-points?walletAddress=${address}`);
+      const pointsResponse = await axios.get(`https://www.data-openblocklabs.com/scroll/wallet-points?walletAddress=${address}`);
+      result.points = pointsResponse.data[0].points;
+      result.timestamp = pointsResponse.data[0].timestamp;
+    } catch (error) {
+      result.points = 0;
+      result.timestamp = 0;
+    }
+
+    return result;
+  }));
+
+  res.send(results);
+
+  console.log("FC Invoke End RequestId: " + requestId);
+});
+
+app.post("/getWalletInfo2", async (req, res) => {
+  const requestId = req.headers["x-fc-request-id"];
+  console.log("FC Invoke Start RequestId: " + requestId);
+
+  const walletAddresses = req.body.wallet_addresses;
+
+  if (!Array.isArray(walletAddresses) || walletAddresses.some(address => !address.startsWith("0x"))) {
+    res.status(400).send({
+      msg: "Invalid wallet_addresses",
+    });
+    return;
+  }
+
+  const results = await Promise.all(walletAddresses.map(async (address) => {
+    const result = { wallet_address: address };
+
+    try {
+      const pointsResponse = await axios.get(`https://www.data-openblocklabs.com/scroll/wallet-points-s2?walletAddress=${address}`);
       result.points = pointsResponse.data[0].points;
       result.timestamp = pointsResponse.data[0].timestamp;
     } catch (error) {
